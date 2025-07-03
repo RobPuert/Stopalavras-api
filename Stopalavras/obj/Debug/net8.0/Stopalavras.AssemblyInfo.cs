f631526f63a2bcf04876cd511c67ce4210f21eff
ROBSON F PUERT <rob_puert@hotmail.com>
2025-07-03 01:10:18 -0300

    atualizacao .net

--- Stopalavras/obj/Debug/net8.0/Stopalavras.AssemblyInfo.cs
+++ Stopalavras/obj/Debug/net8.0/Stopalavras.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Stopalavras")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cfff729fc2d66c8067af7e7caf68894908543773")]
 [assembly: System.Reflection.AssemblyProductAttribute("Stopalavras")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Stopalavras")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
